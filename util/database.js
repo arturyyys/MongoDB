7ad7a42a1dec5959a8fd28646ce6d0586284d289
Artūras <arturyyys@gmail.com>
2024-09-13 13:53:47 +0300

    Finishing the Database Connection

--- util/database.js
+++ util/database.js
@@ -1,17 +1,27 @@
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 
+let_db;
+
 const mongoConnect = (callback) => {
   MongoClient.connect(
-    "mongodb+srv://arturyyys:Astuoni88888888@cluster0.pozcl.mongodb.net/"
+    "mongodb+srv://arturyyys:Astuoni88888888@cluster0.pozcl.mongodb.net/shop?retryWrites=true"
   )
     .then((client) => {
       console.log("Connected to Mongo");
+      _db = client.db();
       callback(client);
     })
     .catch((err) => {
       console.log(err);
+      throw err;
     });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) return _db;
+  throw "No database found!";
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
