3d16703fc088476afa30b11c7e113f1db73d9915
Artūras <arturyyys@gmail.com>
2024-09-14 12:51:43 +0300

    update find product

--- controllers/shop.js
+++ controllers/shop.js
@@ -16,6 +16,15 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
+  // Product.findByPk(prodId)
+  //   .then((product) => {
+  //     res.render("shop/product-detail", {
+  //       product: product,
+  //       pageTitle: product.title,
+  //       path: "/products",
+  //     });
+  //   })
+  //   .catch((err) => console.log(err));
   Product.findByPk(prodId)
     .then((product) => {
       res.render("shop/product-detail", {
@@ -24,7 +33,9 @@ exports.getProduct = (req, res, next) => {
         path: "/products",
       });
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 exports.getIndex = (req, res, next) => {
