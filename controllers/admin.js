1550200feeedbb831a8b676333f423e0e09b97a5
Artūras <arturyyys@gmail.com>
2024-09-14 14:31:00 +0300

    Making the "Edit" & "Delete" Buttons Work Again

--- controllers/admin.js
+++ controllers/admin.js
@@ -71,18 +71,23 @@ exports.postAddProduct = (req, res, next) => {
 //     .catch((err) => console.log(err));
 // };
 
-// exports.getProducts = (req, res, next) => {
-//   req.user
-//     .getProducts()
-//     .then((products) => {
-//       res.render("admin/products", {
-//         prods: products,
-//         pageTitle: "Admin Products",
-//         path: "/admin/products",
-//       });
-//     })
-//     .catch((err) => console.log(err));
-// };
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll()
+    .then((products) => {
+      res.render("admin/products", {
+        prods: products,
+        pageTitle: "Admin Products",
+        path: "/admin/products",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).render("shop/500", {
+        pageTitle: "Error",
+        path: "/500",
+      });
+    });
+};
 
 // exports.postDeleteProduct = (req, res, next) => {
 //   const prodId = req.body.productId;
