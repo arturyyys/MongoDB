5fb518309d274eefee43cc9138aa88434d166783
Artūras <arturyyys@gmail.com>
2024-09-15 20:54:13 +0300

    Deleting Products

--- models/product.js
+++ models/product.js
@@ -61,6 +61,18 @@ class Product {
         console.log(err);
       });
   }
+  static deleteByPk(prodId) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .deleteOne({ _id: new mongodb.ObjectId(prodId) })
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 }
 
 module.exports = Product;
