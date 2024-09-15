fd69a21dc896630800d36f5994af25f07dd4f200
Artūras <arturyyys@gmail.com>
2024-09-15 21:12:23 +0300

    Creating New Users

--- models/user.js
+++ models/user.js
@@ -1,16 +1,28 @@
-const Sequelize = require("sequelize");
+const mongodb = require("mongodb");
+const db = require("../util/database").getDb;
 
-const sequelize = require("../util/database");
+const ObjectId = mongodb.ObjectId;
 
-const User = sequelize.define("user", {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING,
-});
+class User {
+  constructor(name, email) {
+    this.name = name;
+    this.email = email;
+  }
+  save() {
+    const db = getDb();
+    return db.collection("users").insertOne(this);
+  }
+  static findByPk(userId) {
+    const db = getDb();
+    return db.collection("users").findOne({ _id: new ObjectId(userId) });
+    // .next()
+    // .then((user) => {
+    //   if (user) {
+    //     user._id = user._id.toString();
+    //   }
+    //   return user;
+    // });
+  }
+}
 
 module.exports = User;
